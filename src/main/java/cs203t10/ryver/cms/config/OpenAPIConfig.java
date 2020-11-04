e2afb1ad20265a687ff112b283d25891100b7886
kohuining <huining.ko.2019@sis.smu.edu.sg>
2020-11-04 11:44:23 +0800

    Add springdoc config

---
+++ src/main/java/cs203t10/ryver/cms/config/OpenAPIConfig.java
@@ -0,0 +1,32 @@
+package cs203t10.ryver.cms.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Ryver CMS", version = "0.0.1", description = "The content management service for Ryver Bank API"))
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenAPIConfig {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+            .group("Ryver CMS")
+            .pathsToMatch("/**")
+            .build();
+    }
+
+}
+
+
